bf7ea0ea548d686320ea86f42c3706464bdd53a9
Adam Gontarek <adam.gontarek@snpgroup.com>
2019-06-06 15:13:01 +0200

    Added spring boot spark app

---
+++ spark-cassandra-poc-api/src/main/java/com/deelef/sparkcassandrapocapi/SparkCassandraPocApiApplication.java
@@ -0,0 +1,13 @@
+package com.deelef.sparkcassandrapocapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkCassandraPocApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkCassandraPocApiApplication.class, args);
+	}
+
+}
